030fa55263f00840a2c85403886c154830ec4b03
Natan Bensoussan <arielbs@gmail.com>
2024-10-14 14:23:10 +0300

    Remove none helm app

--- argocd.sh
+++ argocd.sh
@@ -18,19 +18,16 @@ brew list k3d >/dev/null || brew install k3d
 echo K3D Create cluster with the registry
 k3d cluster create cluster-argo --port '8080:80@loadbalancer' --port '8443:443@loadbalancer' --port '8090:8090@loadbalancer'
 
-echo Create ArgoCD
-kubectl create namespace argocd
+# echo Create ArgoCD
 helm repo add argocd https://dandydeveloper.github.io/charts/
 cd helm
 helm dependency build
 cd -
 helm upgrade --install -n argocd argocd helm --create-namespace
 
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 # Get server IP
 IP=$(ipconfig getifaddr en0)
-# Patch the aergocd service as a load balancer using the server's IP
+# Patch the argocd service as a load balancer using the server's IP
 kubectl patch svc argocd-server -n argocd -p '{"spec" : {"type": "LoadBalancer", "externalIPs": ["'${IP}'"]}}'
 
 # ArgoCD cli
@@ -63,8 +60,8 @@ echo Create argo application go-server
 user=natanbs
 echo Create secret for ghcr.io
 kubectl create ns app-ns
-kubectl create secret -n app-ns docker-registry ghcr-login-secret --docker-server=https://ghcr.io --docker-username=natanbs --docker-password=${token}
-argocd repo add https://github.com/natanbs/go-server --username natanbs --password $token
-# helm upgrade --install -n app-ns go-server oci://ghcr.io/natanbs/go-server
+kubectl create secret -n app-ns docker-registry ghcr-login-secret --docker-server=https://ghcr.io --docker-username=${user} --docker-password=${token}
+argocd repo add https://github.com/${user}/go-server --username ${user} --password $token
+# helm upgrade --install -n app-ns go-server oci://ghcr.io/${user}/go-server
 
 kubectl apply -f argo-app-go-server/go-server-argo-app.yaml
